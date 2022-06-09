e71702260d260295144d4d13df135c659834c0cb
hchen256@my.bcit.ca <hchen256@my.bcit.ca>
2022-06-08 19:05:10 -0700

    Added url setting

--- twitter/settings.py
+++ twitter/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-]
+    # project apps
+    'accounts',
+    ]
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
