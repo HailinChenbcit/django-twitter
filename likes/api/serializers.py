5c941190833da28b7c8be5f2bb31d29395ef9ac3
hchen256@my.bcit.ca <hchen256@my.bcit.ca>
2022-08-24 15:45:59 -0700

    Added modified urls

--- likes/api/serializers.py
+++ likes/api/serializers.py
@@ -1,51 +1,49 @@
 from accounts.api.serializers import UserSerializer
 from comments.models import Comment
+from django.contrib.contenttypes.models import ContentType
+from likes.models import Like
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from tweets.models import Tweet
 
 
-class CommentSerializer(serializers.ModelSerializer):
+class LikeSerializer(serializers.ModelSerializer):
     user = UserSerializer()
 
     class Meta:
-        model = Comment
-        fields = ('id', 'tweet_id', 'user', 'content', 'created_at')
+        model = Like
+        fields = ('user', 'created_at')
 
 
-class CommentSerializerForCreate(serializers.ModelSerializer):
-    # 这两项必须手动添加
-    # 因为默认 ModelSerializer 里只会自动包含 user 和 tweet 而不是 user_id 和 tweet_id
-    tweet_id = serializers.IntegerField()
-    user_id = serializers.IntegerField()
+class LikeSerializerForCreate(serializers.ModelSerializer):
+    content_type = serializers.ChoiceField(choices=['comment', 'tweet'])
+    object_id = serializers.IntegerField()
 
     class Meta:
-        model = Comment
-        fields = ('content', 'tweet_id', 'user_id')
+        model = Like
+        fields = ('content_type', 'object_id')
+
+    def _get_model_class(self, data):
+        if data['content_type'] == 'comment':
+            return Comment
+        if data['content_type'] == 'tweet':
+            return Tweet
+        return None
 
     def validate(self, data):
-        tweet_id = data['tweet_id']
-        if not Tweet.objects.filter(id=tweet_id).exists():
-            raise ValidationError({'message': 'tweet does not exist'})
-        # 必须 return validated data
-        # 也就是验证过之后，进行过处理的（当然也可以不做处理）输入数据
+        model_class = self._get_model_class(data)
+        if model_class is None:
+            raise ValidationError({'content_type': 'Content type does not exist'})
+        liked_object = model_class.objects.filter(id=data['object_id']).first()
+        if liked_object is None:
+            raise ValidationError({'object_id': 'Object does not exist'})
         return data
 
     def create(self, validated_data):
-        return Comment.objects.create(
-            user_id=validated_data['user_id'],
-            tweet_id=validated_data['tweet_id'],
-            content=validated_data['content'],
+        model_class = self._get_model_class(validated_data)
+        instance, _ = Like.objects.get_or_create(
+            content_type=ContentType.objects.get_for_model(model_class),
+            object_id=validated_data['object_id'],
+            user=self.context['request'].user,
         )
-
-
-class CommentSerializerForUpdate(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = ('content',)
-
-    def update(self, instance, validated_data):
-        instance.content = validated_data['content']
-        instance.save()
-        # update 方法要求 return 修改后的 instance 作为返回值
         return instance
